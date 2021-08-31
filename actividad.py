9a94a8ecc31d381ee8f14a448ea81843e3455464
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-08-31 12:42:46 -0500

    Algoritmo Ejercicio #3

--- actividad.py
+++ actividad.py
@@ -20,3 +20,18 @@ print('La masa es: ', masa)
 edad = input('Digite su edad: ')
 numPulsaciones = (200 - int(edad))/10
 print('El numero de pulsaciones que debe tener es de: ', numPulsaciones)
+
+# 3. Tres personas deciden invertir su dinero para fundar una empresa.
+# Cada una de ellas invierte una cantidad distinta. Obtener el porcentaje
+# que cada quien invierte con respecto a la cantidad total invertida.'''
+p1 = float(input('Digite el valor que va a invertir la 1ra persona: $'))
+p2 = float(input('Digite el valor que va a invertir la 2da persona: $'))
+p3 = float(input('Digite el valor que va a invertir la 3ra persona: $'))
+inversionTotal = p1 + p2 + p3
+porcentajep1 = (p1 / inversionTotal) * 100
+porcentajep2 = (p2 / inversionTotal) * 100
+porcentajep3 = (p3 / inversionTotal) * 100
+print('La inversion total es de $', inversionTotal)
+print(f'El porcentaje de la 1ra es de {porcentajep1}%')
+print(f'El porcentaje de la 2da es de {porcentajep2}%')
+print(f'El porcentaje de la 3ra es de {porcentajep3}%')
