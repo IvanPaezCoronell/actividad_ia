cefc115aacb6411b67118b5bdb8b5311a37dd82e
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-08-31 11:54:31 -0500

    Algoritmo Ejercicio #1

--- actividad.py
+++ actividad.py
@@ -6,3 +6,10 @@ Created on Tue Aug 31 11:10:54 2021
 @author: ivandavid
 """
 
+# 1. Haga un algoritmo que calcule la masa de la siguiente fórmula:
+# Masa = (presión * volúmen) / (0.37 * (temperatura + 460))
+presion = float(input('Digite el valor de la presion: '))
+volumen = float(input('Digite el valor del volumen: '))
+temperatura = float(input('Digite el valor de  la temperatura: '))
+masa = (presion * volumen) / (0.37 * (temperatura + 460))
+print('La masa es: ', masa)
